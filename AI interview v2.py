cf026d62c3cb28be4a75930236dac9670d625729
jerryluo01 <jerryluogd@gmail.com>
2025-03-03 05:03:56 -0500

    Delete AI interview v2.py

--- AI interview v2.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-genre_df = pd.read_csv("genre_data.csv")
-movies_df = pd.read_csv("movies_data.csv", usecols=["imdb_rating"])
-df = pd.concat([genre_df, movies_df], axis=1)
-df1 = df.groupby(["Main_Genre"])[["imdb_rating"]].mean()
-print(df1)
